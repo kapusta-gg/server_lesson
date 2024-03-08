1ac01ae6c693eefde22be6a8a57dc2238be5b10e
bogda <bogdashka04@gmail.com>
2024-03-08 15:23:56 +0300

    commit2

--- server.py
+++ server.py
@@ -9,9 +9,7 @@ app = Flask(__name__)
 def index():
     return "Привет от приложения Flask"
 
-@app.route("/index")
-def index2():
-    return "Привет от приложения Flask2"
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
